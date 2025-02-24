bf386f1397c13e821eaf8540dc514dc440127ec9
Javiuty <javiiescuadra@gmail.com>
2025-02-24 12:01:56 +0100

    fix: fix date + cronjob

--- scrapper.js
+++ scrapper.js
@@ -33,8 +33,10 @@ const saveFlightsData = async (url) => {
   const scrapeData = await page.evaluate(() => {
     const price = document.querySelector('[role="group"] [class*="price-text"]')?.textContent
     const cleanPrice = price.replace(/\s+/g, '')
+    const timestamp = new Date(Date.now())
+    const actualTime = timestamp.toLocaleString('es-ES', { timeZone: 'Europe/Madrid' })
 
-    return { price: cleanPrice, timestamp: Date.now() }
+    return { price: cleanPrice, time: actualTime }
   })
 
   await browser.close()
